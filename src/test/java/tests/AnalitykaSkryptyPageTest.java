b11c22138d47131f74070b38a952cbac13cac7b7
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-04-17 00:13:03 +0200

    dodano strony AnalitykaSkryptyPage i AnalitykaSkryptyPageTest

---
+++ src/test/java/tests/AnalitykaSkryptyPageTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.AnalitykaSkryptyPage;
+import pages.LoginPageNew;
+import pages.PanelPage;
+import pages.UstawieniaPage;
+
+
+public class AnalitykaSkryptyPageTest extends TestBase  {
+
+    /****************sekja techniczna START **********************************************/
+
+
+
+    private LoginPageNew loginPageNew;
+    private PanelPage panelPage;
+    // Obiekt reprezentujący stronę ustawień
+    private UstawieniaPage ustawieniaPage;
+    // Obiekt reprezentujący stronę "AnalitykaSkrypty"
+    private AnalitykaSkryptyPage avalitykaSkryptyPage;
+    /*****************sekja techniczna KONIEC **********************************************/
+
+    @BeforeMethod
+    public void setUpAnalitykaSkryptyPage(){
+        // Metoda uruchamiana przed każdym testem
+        // Inicjalizacja obiektów stron potrzebnych do wykonania testów
+        loginPageNew = new LoginPageNew(driver);
+        panelPage = new PanelPage(driver);
+        ustawieniaPage = new UstawieniaPage(driver);
+        avalitykaSkryptyPage = new AnalitykaSkryptyPage(driver);
+    }
+
+
+
+    // Test weryfikujący, czy adres URL strony "AnalitykaSkrypty" jest poprawny
+    @Test(priority = 110, enabled = true, description = "Weryfikacja url strony AnalitykaSkrypty")
+    public void zweryfikujPoprawnyUrlAnalitykaSkrypty() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkAnalitykaSkrypty();
+        Assert.assertEquals(avalitykaSkryptyPage.getAktualnyUrlAnalitykaSkrypty(), avalitykaSkryptyPage.getPoprawnyUrlAnalitykaSkrypty(),  "Url strony AnalitykaSkrypty nie jest poprawny");
+    }
+
+    // Test weryfikujący, czy tytuł strony "AnalitykaSkrypty" jest poprawny
+    @Test(priority = 120, enabled = true, description = "Weryfikacja tytulu strony AnalitykaSkrypty")
+    public void zweryfikujPoprawnyTytulAnalitykaSkrypty() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkAnalitykaSkrypty();
+        Assert.assertEquals(avalitykaSkryptyPage.getAktualnyTytulAnalitykaSkrypty(), avalitykaSkryptyPage.getPoprawnyTytulAnalitykaSkrypty(),  "Tytul strony AnalitykaSkrypty nie jest poprawny");
+    }
+
+
+
+
+
+
+
+}

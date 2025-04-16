1c60ca3d56e6cc0e720676ee886c0c860d96f237
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-04-17 00:48:49 +0200

    dodano strony ZakupyNaPrezentPage i ZakupyNaPrezentPageTest

---
+++ src/test/java/pages/ZakupyNaPrezentPage.java
@@ -0,0 +1,93 @@
+ package pages;
+
+import helpers.Waits;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+ public class ZakupyNaPrezentPage {
+
+    /************************Seckja techniczno konfiguracyjna START **********************************************/
+
+
+    // Obiekt WebDriver, służący do sterowania przeglądarką
+    private WebDriver driver;
+
+     // Obiekt klasy Waits, służący do obsługi  oczekiwań
+    private Waits wait;
+
+     // Konstruktor klasy Zakupy Na Prezent Page
+     // Inicjalizuje driver oraz obiekt klasy Waits
+     // Inicjalizuje wszystkie elementy strony za pomocą PageFactory
+    public ZakupyNaPrezentPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new Waits(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    /************************Seckja techniczno konfiguracyjna KONIEC**********************************************/
+
+
+    /************************Repozytorium webelementów START **********************************************/
+
+    // Oczekiwany (poprawny) adres URL strony "Zakupy Na Prezent"
+    private String poprawnyUrlZakupyNaPrezent = "https://mmrmqpr585.publigo.onl/wp-admin/admin.php?page=wp-idea-settings&autofocus=gift";
+
+    // Oczekiwany (poprawny) tytuł strony "Zakupy Na Prezent"
+    private String poprawnyTytulZakupyNaPrezent = "Ustawienia ‹ Platforma kursów online — WordPress";
+
+
+
+
+
+
+
+
+
+    /***************************Repozytorium webelementów KONIEC ******************************************/
+
+
+
+
+
+
+    /****************************Operacje na webelementach START **********************************************/
+
+
+    // Zwraca oczekiwany (poprawny) URL strony "Zakupy Na Prezent" i wypisuje go w konsoli
+    public String getPoprawnyUrlZakupyNaPrezent() {
+        System.out.println("Poprawny url strony Zakupy Na Prezent: " + poprawnyUrlZakupyNaPrezent);
+        return poprawnyUrlZakupyNaPrezent;
+    }
+
+     // Zwraca aktualny URL przeglądanej strony "Zakupy Na Prezent" i wypisuje go w konsoli
+    public String getAktualnyUrlZakupyNaPrezent() {
+        System.out.println("Aktualny url strony Zakupy Na Prezent: " + driver.getCurrentUrl());
+        return driver.getCurrentUrl();
+    }
+
+     // Zwraca oczekiwany (poprawny) tytuł strony "Zakupy Na Prezent" i wypisuje go w konsoli
+     public String getPoprawnyTytulZakupyNaPrezent() {
+         System.out.println("Poprawny tytul strony Zakupy Na Prezent: " + poprawnyTytulZakupyNaPrezent);
+         return poprawnyTytulZakupyNaPrezent;
+     }
+
+     // Zwraca aktualny tytuł przeglądanej strony "Zakupy Na Prezent" i wypisuje go w konsoli
+     public String getAktualnyTytulZakupyNaPrezent() {
+         System.out.println("Aktualny tytul strony Zakupy Na Prezent: " + driver.getTitle());
+         return driver.getTitle();
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**********************************Operacje na webelementach KONIEC ******************************************/
+
+}
---
+++ src/test/java/tests/ZakupyNaPrezentPageTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPageNew;
+import pages.PanelPage;
+import pages.UstawieniaPage;
+import pages.ZakupyNaPrezentPage;
+
+
+public class ZakupyNaPrezentPageTest extends TestBase  {
+
+    /****************sekja techniczna START **********************************************/
+
+
+    private LoginPageNew loginPageNew;
+    private PanelPage panelPage;
+    // Obiekt reprezentujący stronę ustawień
+    private UstawieniaPage ustawieniaPage;
+    // Obiekt reprezentujący stronę "Zakupy Na Prezent"
+    private ZakupyNaPrezentPage zakupyNaPrezentPage;
+    /*****************sekja techniczna KONIEC **********************************************/
+
+    @BeforeMethod
+    public void setUpZakupyNaPrezentPage(){
+        // Metoda uruchamiana przed każdym testem
+        // Inicjalizacja obiektów stron potrzebnych do wykonania testów
+        loginPageNew = new LoginPageNew(driver);
+        panelPage = new PanelPage(driver);
+        ustawieniaPage = new UstawieniaPage(driver);
+        zakupyNaPrezentPage = new ZakupyNaPrezentPage(driver);
+    }
+
+
+
+    // Test weryfikujący, czy adres URL strony "Zakupy Na Prezent" jest poprawny
+    @Test(priority = 110, enabled = true, description = "Weryfikacja url strony Zakupy Na Prezent")
+    public void zweryfikujPoprawnyUrlZakupyNaPrezent() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkZakupyNaPrezent();
+        Assert.assertEquals(zakupyNaPrezentPage.getAktualnyUrlZakupyNaPrezent(), zakupyNaPrezentPage.getPoprawnyUrlZakupyNaPrezent(),  "Url strony Zakupy Na Prezent nie jest poprawny");
+    }
+
+    // Test weryfikujący, czy tytuł strony "Zakupy Na Prezent" jest poprawny
+    @Test(priority = 120, enabled = true, description = "Weryfikacja tytulu strony Zakupy Na Prezent")
+    public void zweryfikujPoprawnyTytulZakupyNaPrezent() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkZakupyNaPrezent();
+        Assert.assertEquals(zakupyNaPrezentPage.getAktualnyTytulZakupyNaPrezent(), zakupyNaPrezentPage.getPoprawnyTytulZakupyNaPrezent(),  "Tytul strony Zakupy Na Prezent nie jest poprawny");
+    }
+
+
+
+
+
+
+
+}

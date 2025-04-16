62cf3058417da1f3f20f6c62e598e4f7957b3cc0
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2025-04-17 00:21:26 +0200

    dodano strony KoszykZakupowyPage i KoszykZakupowyPageTest

---
+++ src/test/java/pages/KoszykZakupowyPage.java
@@ -0,0 +1,93 @@
+ package pages;
+
+import helpers.Waits;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+ public class KoszykZakupowyPage {
+
+    /************************Seckja techniczno konfiguracyjna START **********************************************/
+
+
+    // Obiekt WebDriver, służący do sterowania przeglądarką
+    private WebDriver driver;
+
+     // Obiekt klasy Waits, służący do obsługi  oczekiwań
+    private Waits wait;
+
+     // Konstruktor klasy koszykZakupowyPage
+     // Inicjalizuje driver oraz obiekt klasy Waits
+     // Inicjalizuje wszystkie elementy strony za pomocą PageFactory
+    public KoszykZakupowyPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new Waits(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    /************************Seckja techniczno konfiguracyjna KONIEC**********************************************/
+
+
+    /************************Repozytorium webelementów START **********************************************/
+
+    // Oczekiwany (poprawny) adres URL strony "Koszyk Zakupowy"
+    private String poprawnyUrlKoszykZakupowy = "https://mmrmqpr585.publigo.onl/wp-admin/admin.php?page=wp-idea-settings&autofocus=cart";
+
+    // Oczekiwany (poprawny) tytuł strony "Koszyk Zakupowy"
+    private String poprawnyTytulKoszykZakupowy = "Ustawienia ‹ Platforma kursów online — WordPress";
+
+
+
+
+
+
+
+
+
+    /***************************Repozytorium webelementów KONIEC ******************************************/
+
+
+
+
+
+
+    /****************************Operacje na webelementach START **********************************************/
+
+
+    // Zwraca oczekiwany (poprawny) URL strony "Koszyk Zakupowy" i wypisuje go w konsoli
+    public String getPoprawnyUrlKoszykZakupowy() {
+        System.out.println("Poprawny url strony koszyk Zakupowy: " + poprawnyUrlKoszykZakupowy);
+        return poprawnyUrlKoszykZakupowy;
+    }
+
+     // Zwraca aktualny URL przeglądanej strony "Koszyk Zakupowy" i wypisuje go w konsoli
+    public String getAktualnyUrlKoszykZakupowy() {
+        System.out.println("Aktualny url strony koszyk Zakupowy: " + driver.getCurrentUrl());
+        return driver.getCurrentUrl();
+    }
+
+     // Zwraca oczekiwany (poprawny) tytuł strony "Koszyk Zakupowy" i wypisuje go w konsoli
+     public String getPoprawnyTytulKoszykZakupowy() {
+         System.out.println("Poprawny tytul strony koszyk Zakupowy: " + poprawnyTytulKoszykZakupowy);
+         return poprawnyTytulKoszykZakupowy;
+     }
+
+     // Zwraca aktualny tytuł przeglądanej strony "Koszyk Zakupowy" i wypisuje go w konsoli
+     public String getAktualnyTytulKoszykZakupowy() {
+         System.out.println("Aktualny tytul strony koszyk Zakupowy: " + driver.getTitle());
+         return driver.getTitle();
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**********************************Operacje na webelementach KONIEC ******************************************/
+
+}
---
+++ src/test/java/tests/KoszykZakupowyPageTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.KoszykZakupowyPage;
+import pages.LoginPageNew;
+import pages.PanelPage;
+import pages.UstawieniaPage;
+
+
+public class KoszykZakupowyPageTest extends TestBase  {
+
+    /****************sekja techniczna START **********************************************/
+
+
+
+
+    private LoginPageNew loginPageNew;
+    private PanelPage panelPage;
+    // Obiekt reprezentujący stronę ustawień
+    private UstawieniaPage ustawieniaPage;
+    // Obiekt reprezentujący stronę "Koszyk Zakupowy"
+    private KoszykZakupowyPage koszykZakupowyPage;
+    /*****************sekja techniczna KONIEC **********************************************/
+
+    @BeforeMethod
+    public void setUpKoszykZakupowyPage(){
+        // Metoda uruchamiana przed każdym testem
+        // Inicjalizacja obiektów stron potrzebnych do wykonania testów
+        loginPageNew = new LoginPageNew(driver);
+        panelPage = new PanelPage(driver);
+        ustawieniaPage = new UstawieniaPage(driver);
+        koszykZakupowyPage = new KoszykZakupowyPage(driver);
+    }
+
+
+
+    // Test weryfikujący, czy adres URL strony "Koszyk Zakupowy" jest poprawny
+    @Test(priority = 110, enabled = true, description = "Weryfikacja url strony koszyk Zakupowy")
+    public void zweryfikujPoprawnyUrlKoszykZakupowy() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkKoszykZakupowy();
+        Assert.assertEquals(koszykZakupowyPage.getAktualnyUrlKoszykZakupowy(), koszykZakupowyPage.getPoprawnyUrlKoszykZakupowy(),  "Url strony koszyk Zakupowy nie jest poprawny");
+    }
+
+    // Test weryfikujący, czy tytuł strony "Koszyk Zakupowy" jest poprawny
+    @Test(priority = 120, enabled = true, description = "Weryfikacja url strony koszyk Zakupowy")
+    public void zweryfikujPoprawnyTytulKoszykZakupowy() {
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijPrzyciskUstawienia();
+        ustawieniaPage.kliknijLinkKoszykZakupowy();
+        Assert.assertEquals(koszykZakupowyPage.getAktualnyTytulKoszykZakupowy(), koszykZakupowyPage.getPoprawnyTytulKoszykZakupowy(),  "Tytul strony koszyk Zakupowy nie jest poprawny");
+    }
+
+
+
+
+
+
+
+}

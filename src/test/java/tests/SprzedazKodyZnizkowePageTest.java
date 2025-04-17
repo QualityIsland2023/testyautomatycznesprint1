92a1d0bbf47f24f2d575738886f964aaee249808
Agnieszka Gaj <agnieszka.a.gaj@outlook.com>
2025-04-17 15:10:30 +0200

    Dodano wszystkie testy dla zakładki Sprzedaż: SprzedazHistoriaCenPageTest , SprzedazKlienciPageTest , SprzedazKodyZnizkowePageTest , SprzedazPlatnosciZaplanowanePageTest , SprzedazUtworzPlatnoscPageTest , SprzedazZamowieniaPageTest

---
+++ src/test/java/tests/SprzedazKodyZnizkowePageTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class SprzedazKodyZnizkowePageTest extends TestBase {
+
+    /**************** Sekcja techniczna START **********************************************/
+
+    private LoginPageNew loginPageNew;
+    private PanelPage panelPage;
+    private SprzedazKodyZnizkowePage sprzedazKodyZnizkowePage; //instancja strony logowania
+
+    /***************** Sekcja techniczna KONIEC **********************************************/
+
+    @BeforeMethod
+    public void setUPLoginPage(){
+        //inicjalizacja strony logowania , linijka techniczna, konfiguracyjna
+        loginPageNew = new LoginPageNew(driver);
+        panelPage = new PanelPage(driver);
+        sprzedazKodyZnizkowePage = new SprzedazKodyZnizkowePage(driver);
+    }
+
+    @Test(priority = 10, enabled = true, description = "Weryfikacja tytyłu strony KODY ZNIŻKOWE")
+    public void weryfikacjaTytuluStronyKodyZnizkowe(){
+
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijSprzedazKodyZnizkoweMenu();
+
+        Assert.assertEquals(sprzedazKodyZnizkowePage.zwrocAktualnyTytulStronyKodyZnizkowe(),
+                sprzedazKodyZnizkowePage.zwrocPoprawnyTytulStronyKodyZnizkowe(),
+                "Tytuł strony KODY ZNIŻKOWE nie jest poprawny");
+    }
+
+    @Test(priority = 20, enabled = true, description = "Weryfikacja poprawnego adresu strony KODY ZNIŻKOWE"  )
+    public void weryfikacjaPoprawnegoAdresuStronyKodyZnizkowe(){
+
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijSprzedazKodyZnizkoweMenu();
+
+        Assert.assertEquals(sprzedazKodyZnizkowePage.zwrocAktualnyUrlStronyKodyZnizkowe(),
+                sprzedazKodyZnizkowePage.zwrocPoprawnyUrlStronyKodyZnizkowe(),
+                "Adres url strony KODY ZNIŻKOWE nie jest poprawny");
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/tests/SprzedazPlatnosciZaplanowanePageTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class SprzedazPlatnosciZaplanowanePageTest extends TestBase {
+
+    /**************** Sekcja techniczna START **********************************************/
+
+    private LoginPageNew loginPageNew;
+    private PanelPage panelPage;
+    private SprzedazPlatnosciZaplanowanePage sprzedazPlatnosciZaplanowanePage; //instancja strony logowania
+
+    /***************** Sekcja techniczna KONIEC **********************************************/
+
+    @BeforeMethod
+    public void setUPLoginPage(){
+        //inicjalizacja strony logowania , linijka techniczna, konfiguracyjna
+        loginPageNew = new LoginPageNew(driver);
+        panelPage = new PanelPage(driver);
+        sprzedazPlatnosciZaplanowanePage = new SprzedazPlatnosciZaplanowanePage(driver);
+    }
+
+
+    @Test(priority = 10, enabled = true, description = "Weryfikacja tytyłu strony PŁATNOŚCI ZAPLANOWANE")
+    public void weryfikacjaTytuluStronyPlatnosciZaplanowane(){
+
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijSprzedazPlatnosciZaplanowaneMenu();
+
+        Assert.assertEquals(sprzedazPlatnosciZaplanowanePage.zwrocAktualnyTytulStronyPlatnosciZaplanowane(),
+                sprzedazPlatnosciZaplanowanePage.zwrocPoprawnyTytulStronyPlatnosciZaplanowane(),
+                "Tytuł strony PŁATNOŚCI ZAPLANOWANE nie jest poprawny");
+    }
+
+    @Test(priority = 20, enabled = true, description = "Weryfikacja poprawnego adresu strony PŁATNOŚCI ZAPLANOWANE"  )
+    public void weryfikacjaPoprawnegoAdresuStronyPlatnosciZaplanowane(){
+
+        loginPageNew.wykonajLogowanie();
+        panelPage.kliknijSprzedazPlatnosciZaplanowaneMenu();
+
+        Assert.assertEquals(sprzedazPlatnosciZaplanowanePage.zwrocAktualnyUrlStronyPlatnosciZaplanowane(),
+                sprzedazPlatnosciZaplanowanePage.zwrocPoprawnyUrlStronyPlatnosciZaplanowane(),
+                "Adres url strony PŁATNOŚCI ZAPLANOWANE nie jest poprawny");
+    }
+
+
+
+
+
+
+
+
+
+}

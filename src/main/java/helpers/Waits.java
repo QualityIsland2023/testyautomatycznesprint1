e3639720acb26815ed0744741031de2c2fd30794
maralarum <kondrackee2@gmail.com>
2025-04-24 23:44:33 +0200

    Dodano metode waitForInvisibility w klasie Waits

--- src/main/java/helpers/Waits.java
+++ src/main/java/helpers/Waits.java
@@ -47,4 +47,8 @@ public class Waits {
         return wait.until(driver -> driver.getPageSource().contains(text));
     }
 
+    public boolean waitForInvisibility(WebElement element) {
+        return wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
 }
